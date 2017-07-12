77838898f53d59da82c73b6e51d452262bb4b7b7
Andrew Melfi <melfi@umich.edu>
2017-07-12 16:17:58 -0400

    Added tolerance optimization to onlydouble and notriple. (Effectively changes from O(n^3) to O(n^2))
    Additionally, cleaned the repository of unwanted files.

--- lsample/c_simm.cpp
+++ lsample/c_simm.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+
+#define ZERO_TOL 1e-120
+
 using namespace std;
 
 static double *gl_idbl=NULL;
@@ -11,6 +14,7 @@ static double *gl_altby2N=NULL;
 static double *gl_onemiby2N=NULL;
 static long gl_nmax = -1000;
 static long gl_N = -1000;
+static bool gl_optflag = true;
 /*
  * gl_state 0: variables uninitialized not allocated.
  *          1: variables uninitialized and allocated
@@ -19,61 +23,73 @@ static long gl_N = -1000;
 static long gl_state = 0;
 
 static void alloc(long n){
-	gl_idbl =(double *)_mm_malloc((n+1)*sizeof(double), 16);
-	gl_altby2N = (double *)_mm_malloc((n+1)*sizeof(double), 16);
-	gl_onemiby2N = (double *)_mm_malloc((n+1)*sizeof(double), 16);
+    gl_idbl =(double *)_mm_malloc((n+1)*sizeof(double), 16);
+    gl_altby2N = (double *)_mm_malloc((n+1)*sizeof(double), 16);
+    gl_onemiby2N = (double *)_mm_malloc((n+1)*sizeof(double), 16);
 }
 
 static void dealloc(){
-	_mm_free(gl_idbl);
-	_mm_free(gl_altby2N);
-	_mm_free(gl_onemiby2N);
+    _mm_free(gl_idbl);
+    _mm_free(gl_altby2N);
+    _mm_free(gl_onemiby2N);
+    gl_idbl=NULL;
+    gl_altby2N=NULL;
+    gl_onemiby2N=NULL;
+    gl_state=0;
+}
+
+void gl_setoptflag(bool b){
+    gl_optflag=b;
+    return;
 }
 
 void gl_setnmax(long n){
-	assrt(n > 0);
-	switch(gl_state){
-	case 0:
-		assrt(gl_idbl==NULL);
-		assrt(gl_altby2N==NULL);
-		assrt(gl_onemiby2N==NULL);
-		gl_nmax = n;
-		gl_N = -1000;
-		alloc(gl_nmax);
-		gl_state = 1;
-		break;
-	case 1:
-	case 2:
-		if(gl_nmax >= n)
-			return;
-		else{
-			dealloc();
-			gl_nmax = n;
-			gl_N = -1000;
-			alloc(gl_nmax);
-			gl_state = 1;
-		}
-		break;
-	default:
-		assrt(0==1);
-		break;
+    assrt(n > 0);
+    switch(gl_state){
+    case 0:
+    	assrt(gl_idbl==NULL);
+    	assrt(gl_altby2N==NULL);
+    	assrt(gl_onemiby2N==NULL);
+    	gl_nmax = n;
+    	gl_N = -1000;
+    	alloc(gl_nmax);
+    	gl_state = 1;
+    	break;
+    case 1:
+    case 2:
+    	if(gl_nmax >= n)
+    	    return;
+    	else{
+	    dealloc();
+	    gl_nmax = n;
+	    gl_N = -1000;
+	    alloc(gl_nmax);
+	    gl_state = 1;
 	}
+	break;
+    default:
+    	assrt(0==1);
+    	break;
+    }
+    return;
 }
 
 void gl_setN(long N){
-	assrt((gl_state==1) || (gl_state==2));
-	gl_N = N;
-	long n = gl_nmax;
-	for(long i=0; i < n+1; i++)
-		gl_idbl[i] = i;
-	for(long i=0; i < n+1; i++)
-		if (i%2 == 0)
-			gl_altby2N[i] = 1.0;
-		else
-			gl_altby2N[i] = 1.0/(i+1)/(2*N);
-	for(long i=0; i < n+1; i++)
-		gl_onemiby2N[i] = 1.0- i*1.0/(2*N);
-	gl_state = 2;
+    assrt((gl_state==1) || (gl_state==2));
+    if (gl_N==N) return;
+    gl_N = N;
+    long n = gl_nmax;
+    for(long i=0; i < n+1; i++)
+    	gl_idbl[i] = i;
+    for(long i=0; i < n+1; i++)
+    	if (i%2 == 0)
+    	    gl_altby2N[i] = 1.0;
+	else
+	    gl_altby2N[i] = 1.0/(i+1)/(2*N);
+    for(long i=0; i < n+1; i++)
+	gl_onemiby2N[i] = 1.0- i*1.0/(2*N);
+    gl_state = 2;
+    return;
 }
 
 /*
@@ -83,25 +99,22 @@ void gl_setN(long N){
  * Returns probability of k double collisions in single bwd WF step.
  */
 
-double collprob_opt(long k, long n, long N)
+double collprob_opt(long k, long n)
 {
-	double p[2] = {1.0, 1.0};
-	double *restrict idbl = gl_idbl;
-	double *restrict altby2N = gl_altby2N;
-	double *restrict onemiby2N = gl_onemiby2N;
-    
-	//n*(n-1)..(n-2k+1)*(2k-1)..3.1/(2k)!/(2N)**k
-	for (long i=0;i<2*k;i=i+2){
-		p[0] *= idbl[n-i];
-		p[0] *= altby2N[i];
-		p[1] *= idbl[n-i-1];
-		p[1] *= altby2N[i+1];
-	}
-	double pp = p[0]*p[1];
-	for (long i=n-k-1;i>0;i--){
-		pp *= onemiby2N[i];
-	}
-	return pp;
+    double p=1.0;
+    double *restrict idbl = gl_idbl;
+    double *restrict altby2N = gl_altby2N;
+    double *restrict onemiby2N = gl_onemiby2N;
+
+    //n*(n-1)..(n-2k+1)*(2k-1)..3.1/(2k)!/(2N)**k
+    for (long i=0;i<2*k;i++){
+    	p *= idbl[n-i];
+    	p *= altby2N[i];
+    }
+    for (long i=n-k-1;i>0;i--){
+    	p *= onemiby2N[i];
+    }
+    return p;
 }
 
 double collprob_unopt(long k, long n, long N)
@@ -111,12 +124,12 @@ double collprob_unopt(long k, long n, long N)
 
     //n*(n-1)..(n-2k+1)*(2k-1)..3.1/(2k)!/(2N)**k
     for (long i=0;i<2*k;i++) {
-	    outp*=double(n-i);
-	    if (i%2==1) 
-		    outp/=double(i+1)*twon;
+        outp*=double(n-i);
+        if (i%2==1) 
+    	    outp/=double(i+1)*twon;
     }
     for (long i=n-k-1;i>0;i--){
-	    outp*=1-double(i)/twon;
+        outp*=1-double(i)/twon;
     }
     return outp;
 }
@@ -124,10 +137,10 @@ double collprob_unopt(long k, long n, long N)
 extern "C"
 double collprob(long k, long n, long N)
 {
-	if (n > gl_nmax)
-		gl_setnmax(n);
-	gl_setN(N);
-	return collprob_opt(k, n, N);
+    if (n > gl_nmax)
+    	gl_setnmax(n);
+    gl_setN(N);
+    return collprob_opt(k, n);
 }
 
 
@@ -144,10 +157,12 @@ void onlydouble(long n, double* output, long* N, long maxgens, double tol)
     double * lastgen = new double[n+1];
     double * nextgen = new double[n+1];
     double * tmp = 0;
+    gl_setnmax(n);
     for (long i=0;i<n+1;i++)
         lastgen[i]=0;
     lastgen[n]=1;
     for (long i=0;i<maxgens;i++) {
+        gl_setN(N[i]);
         for (long k=1;k<n+1;k++) {
             nextgen[k]=0;
             for (long l=k;l<k+2;l++) {
@@ -156,7 +171,8 @@ void onlydouble(long n, double* output, long* N, long maxgens, double tol)
                     nextgen[1]=lastgen[1];
                     continue;
                 }
-                nextgen[k]+=lastgen[l]*collprob(l-k,l,N[i]);
+                if (lastgen[l]< ZERO_TOL &&gl_optflag) continue;
+                nextgen[k]+=lastgen[l]*collprob_opt(l-k,l);
             }
         }
         nextgen[0]=1;
@@ -171,6 +187,7 @@ void onlydouble(long n, double* output, long* N, long maxgens, double tol)
                 output[j]=lastgen[j];
             delete lastgen;
             delete nextgen;
+            dealloc();
             cout<<"Finished on generation "<<i<<endl;
             return;
         }
@@ -180,6 +197,7 @@ void onlydouble(long n, double* output, long* N, long maxgens, double tol)
         output[j]=lastgen[j];
     delete lastgen;
     delete nextgen;
+    dealloc();
     return;
 }
 
@@ -189,10 +207,12 @@ void notriple(long n, double* output, long* N, long maxgens, double tol)
     double * lastgen = new double[n+1];
     double * nextgen = new double[n+1];
     double * tmp = 0;
+    gl_setnmax(n);
     for (long i=0;i<n+1;i++)
         lastgen[i]=0;
     lastgen[n]=1;
     for (long i=0;i<maxgens;i++) {
+        gl_setN(N[i]);
         for (long k=1;k<n+1;k++) {
             nextgen[k]=0;
             for (long l=k;l<n+1;l++) {
@@ -200,7 +220,8 @@ void notriple(long n, double* output, long* N, long maxgens, double tol)
                     nextgen[1]=lastgen[1];
                     continue;
                 }
-                nextgen[k]+=lastgen[l]*collprob(l-k,l,N[i]);
+                if (lastgen[l]< ZERO_TOL &&gl_optflag) continue;
+                nextgen[k]+=lastgen[l]*collprob_opt(l-k,l);
             }
         }
         nextgen[0]=1;
@@ -215,6 +236,7 @@ void notriple(long n, double* output, long* N, long maxgens, double tol)
                 output[j]=lastgen[j];
             delete lastgen;
             delete nextgen;
+            dealloc();
             cout<<"Finished on generation "<<i<<endl;
             return;
         }
@@ -224,7 +246,6 @@ void notriple(long n, double* output, long* N, long maxgens, double tol)
         output[j]=lastgen[j];
     delete lastgen;
     delete nextgen;
+    dealloc();
     return;
 }
-
-
--- lsample/time_simm.cpp
+++ lsample/time_simm.cpp
@@ -3,44 +3,136 @@
 #include "StatVector.hh"
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 
-double collprob_opt(long k, long n, long N);
+double collprob_opt(long k, long n);
 double collprob_unopt(long k, long n, long N);
+
+extern "C"
+void onlydouble(long n, double* output, long* N, long maxgens, double tol);
+
+extern "C"
+void notriple(long n, double* output, long* N, long maxgens, double tol);
+
 void gl_setnmax(long n);
 void gl_setN(long N);
+void gl_setoptflag(bool b);
 
 void time_collp()
 {
-	long count = 8;
-	long ks[] = {1, 2, 1, 20, 1, 200, 1, 2000};
-	long ns[] = {10, 10, 100, 100, 1000, 1000, 10000, 10000};
-	gl_setnmax(500);	
-	long N = 1l*1000*100;
-	gl_setN(N);
+    long count = 8;
+    long ks[] = {1, 2, 1, 20, 1, 200, 1, 2000};
+    long ns[] = {10, 10, 100, 100, 1000, 1000, 10000, 10000};
+    gl_setnmax(500);	
+    long N = 1l*1000*100;
+    gl_setN(N);
 	
-	TimeStamp clk;
-	int ntrial = 100;
-	StatVector stats(ntrial);
-	double ans, cycles;
-	for(long i=0; i < count; i++){
-		long k = ks[i];
-		long n = ns[i];
-		for(long j=0; j < ntrial; j++){
-			clk.tic();
-			ans = collprob_opt(k, n, N);
-			cycles = clk.toc();
-			stats.insert(cycles);
-		}
-		printf("ans: %.2e\n", ans);
-		printf("k, n: %ld, %ld\n", k, n);
-		printf("cycles (median): %.2e\n", stats.median());
-		printf("cycles (min): %.2e\n\n", stats.min());
-		stats.flush();
+    TimeStamp clk;
+    int ntrial = 100;
+    StatVector stats(ntrial);
+    double ans, cycles;
+    for(long i=0; i < count; i++){
+    	long k = ks[i];
+    	long n = ns[i];
+    	for(long j=0; j < ntrial; j++){
+            clk.tic();
+	    ans = collprob_opt(k, n);
+	    cycles = clk.toc();
+	    stats.insert(cycles);
 	}
+	printf("ans: %.2e\n", ans);
+	printf("k, n: %ld, %ld\n", k, n);
+	printf("cycles (median): %.2e\n", stats.median());
+	printf("cycles (min): %.2e\n\n", stats.min());
+	stats.flush();
+    }
+    return;
+}
+
+void triple_speedcomp()
+{
+    TimeStamp clk;
+    double cycles;
+    double cycles2;
+    long N[1000000];
+    for (int i=0;i<1000000;i++) N[i]=10000;
+    double outp[1000];
+    double outp2[1000];
+    long n=40;
+    double tol=.01;
+    printf("Running slow:----------------------\n");
+    clk.tic();
+    gl_setoptflag(false);
+    notriple(n,outp,N,1000000,tol);
+    cycles = clk.toc();
+    printf("Running fast:----------------------\n");
+    clk.tic();
+    gl_setoptflag(true);
+    notriple(n,outp2,N,1000000,tol);
+    cycles2 = clk.toc();
+    printf("For n=%ld, N=%d, notriple:\n",n,10000);
+    printf("cycles for slow version: %.2e\n", cycles);
+    printf("cycles for fast version: %.2e\n", cycles2);
+    double absdiff=0;
+    for (int i=0;i<n;i++)
+        absdiff+=std::abs(outp[i]-outp2[i]);
+    printf("Abs diff between output results: %.3e\n\n\n", absdiff);
+    return;
+}
+
+void double_speedcomp()
+{
+    TimeStamp clk;
+    double cycles;
+    double cycles2;
+    long N[1000000];
+    for (int i=0;i<1000000;i++) N[i]=10000;
+    double outp[1000];
+    double outp2[1000];
+    long n=100;
+    double tol=.01;
+    printf("Running slow:----------------------\n");
+    clk.tic();
+    gl_setoptflag(false);
+    onlydouble(n,outp,N,1000000,tol);
+    cycles = clk.toc();
+    printf("Running fast:----------------------\n");
+    clk.tic();
+    gl_setoptflag(true);
+    onlydouble(n,outp2,N,1000000,tol);
+    cycles2 = clk.toc();
+    printf("For n=%ld, N=%d, onlydouble:\n",n,10000);
+    printf("cycles for slow version: %.2e\n", cycles);
+    printf("cycles for fast version: %.2e\n", cycles2);
+    double absdiff=0;
+    for (int i=0;i<n;i++)
+        absdiff+=std::abs(outp[i]-outp2[i]);
+    printf("Abs diff between output results: %.3e\n\n\n", absdiff);
+    return;
+}
+void triple_speedtest()
+{
+    TimeStamp clk;
+    double cycles;
+    long N[1000000];
+    for (int i=0;i<1000000;i++) N[i]=10000;
+    double outp[1000];
+    long n=200;
+    double tol=.01;
+
+    clk.tic();
+    gl_setoptflag(true);
+    notriple(n,outp,N,1000000,tol);
+    cycles=clk.toc();
+    printf("For n=%ld, N=%d:\n",n,10000);
+    printf("cycles: %.2e\n", cycles);
 }
 
 int main()
 {
-	time_collp();
+    //time_collp();
+    //triple_speedtest();
+    triple_speedcomp();
+    double_speedcomp();
 }
 	
